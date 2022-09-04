3dfe2a75a405afd7b0cf45c7c031e03638f684b3
RonnyK36 <ronnykelvyne3@gmail.com>
2022-09-05 01:49:24 +0300

    Get movies from API and display image, title and rating

---
+++ app.js
@@ -0,0 +1,29 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+
+const IMGPATH = "https://image.tmdb.org/t/p/w1280"
+
+getMovies(APIURL)
+
+
+async function getMovies(url) {
+    const response = await fetch(url)
+    const responseData = await response.json()
+    console.log(responseData);
+
+    responseData.results.forEach(movie => {
+        const { poster_path, title, vote_average } = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+       
+            <img src="${IMGPATH + poster_path}" alt="${title}" srcset="">
+            <div class="movie-info">
+                <h3 class="movie-title">${title}</h3>
+                <span class="movie-rating">${vote_average}</span>
+            </div>
+        
+        `
+
+        document.body.appendChild(movieEl)
+    });
+}
